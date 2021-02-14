674f46732cb3c7eb31340f6515977bd736d6d3e7
Noora Pöysti <noora.poysti@tuni.fi>
2021-02-15 01:41:02 +0200

    added input image argument

--- detect_dog.py
+++ detect_dog.py
@@ -1,76 +1,89 @@
 import cv2
 import numpy as np
 import os
+import argparse
 
-# YOLOv3 weights can be downloaded from: https://pjreddie.com/media/files/yolov3.weights
-##### DEFINE WEIGHT AND MODEL FILES HERE #####
-weights = "bin/yolov3.weights"
-model = "cfg/yolov3.cfg"
-##############################################
+def main(): 
+    # YOLOv3 weights can be downloaded from: https://pjreddie.com/media/files/yolov3.weights
+    ##### DEFINE WEIGHT AND MODEL FILES HERE #####
+    weights = "bin/yolov3.weights"
+    model = "cfg/yolov3.cfg"
+    ##############################################
 
-cwd = os.getcwd()
-weights_dir = os.path.join(cwd, weights)
-model_dir = os.path.join(cwd, model)
+    cwd = os.getcwd()
+    weights_dir = os.path.join(cwd, weights)
+    model_dir = os.path.join(cwd, model)
 
-net = cv2.dnn.readNet(weights_dir, model_dir)
+    net = cv2.dnn.readNet(weights_dir, model_dir)
 
-classes = []
+    parser = argparse.ArgumentParser(description="Detect dog(s) from input image")
+    parser.add_argument("input", metavar="img", type=str, help="path to input image")
+    args = parser.parse_args()
 
-with open("coco.names", "r") as f:    
-    classes = [line.strip() for line in f.readlines()]
+    if os.path.isfile(args.input):
+        image = cv2.imread(args.input)
+    else:
+        print("Image not found")
+        return
 
-layer_names = net.getLayerNames()
-output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    classes = []
 
-##### INPUT IMAGE #####
-image = cv2.imread("dog1.jpg")
+    with open("coco.names", "r") as f:    
+        classes = [line.strip() for line in f.readlines()]
 
-font = cv2.FONT_HERSHEY_PLAIN
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
-height, width, channels = image.shape
+    font = cv2.FONT_HERSHEY_PLAIN
 
-# Detect objects
-blob = cv2.dnn.blobFromImage(image, 0.00392, (416,416), (0,0,0), True, crop=False)
+    height, width, channels = image.shape
 
-net.setInput(blob)
-outs = net.forward(output_layers)
+    # Detect objects
+    blob = cv2.dnn.blobFromImage(image, 0.00392, (416,416), (0,0,0), True, crop=False)
 
-class_id = 0
-confidences = []
-boxes = []
+    net.setInput(blob)
+    outs = net.forward(output_layers)
 
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = float(scores[class_id])
-        
-        if class_id == 16:
-            # Possible dog detected!
-            if confidence > 0.5:
-                w = detection[2] * width
-                h = detection[3] * height
-                x = detection[0] * width - np.rint(w/2)
-                y = detection[1] * height + np.rint(h/2)
+    class_id = 0
+    confidences = []
+    boxes = []
 
-                boxes.append([int(x), int(y), int(w), int(h)])
-                confidences.append(confidence)
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = float(scores[class_id])
+            
+            if class_id == 16:
+                # Possible dog detected!
+                if confidence > 0.5:
+                    w = detection[2] * width
+                    h = detection[3] * height
+                    x = detection[0] * width - np.rint(w/2)
+                    y = detection[1] * height + np.rint(h/2)
 
-indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.8, 0.3)
+                    boxes.append([int(x), int(y), int(w), int(h)])
+                    confidences.append(confidence)
 
-if len(boxes) == 0:
-    print("No dogs found :(")
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.8, 0.3)
 
-else:
-    for i in range(len(boxes)):
-        if i in indexes:
-            x, y, w, h = boxes[i]
-            label = "dog"
-            confidence = confidences[i]
-            color = (0, 255, 0)
+    if len(indexes) == 0:
+        print("No dogs found :(")
 
-            cv2.rectangle(image, (x,y), (x+w, y-h), (0,255,0), 2)
-            cv2.putText(image, label + " " + str(round(confidence,2)), (x, y + 30), font, 3, color, 3)
-   
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
+    else:
+        print(len(indexes), "dog(s) found!")
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = "dog"
+                confidence = confidences[i]
+                color = (0, 255, 0)
+
+                cv2.rectangle(image, (x,y), (x+w, y-h), (0,255,0), 2)
+                cv2.putText(image, label + " " + str(round(confidence,2)), (x, y + 30), font, 3, color, 3)
+       
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+
+if __name__ == "__main__":
+    main()
